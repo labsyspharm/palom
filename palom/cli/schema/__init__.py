645c9ec8daa501b11cc876bbbae8e6bcdda0aad7
kbestak <86408271+kbestak@users.noreply.github.com>
2022-04-13 12:36:53 +0200

    Update __init__.py

--- palom/cli/schema/__init__.py
+++ palom/cli/schema/__init__.py
@@ -4,5 +4,8 @@ import yamale
 current_dir = (pathlib.Path(__file__).resolve().parent)
 svs_config_schema_path = current_dir / 'svs-config-schema.yml'
 svs_config_example_path = current_dir / 'svs-config-example.yml'
+cycif_config_schema_path = current_dir / 'cycif-config-schema.yml'
+cycif_config_example_path = current_dir / 'cycif-config-example.yml'
 
 svs_config_schema = yamale.make_schema(svs_config_schema_path)
+cycif_config_schema = yamale.make_schema(cycif_config_schema_path)
