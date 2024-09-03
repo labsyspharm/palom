2fdccb7b3ce0b006130129fd220dbe914e89dc7a
Yu-An Chen <atwood12@gmail.com>
2024-09-03 15:47:30 -0700

    Format pyramid.py with ruff

--- palom/pyramid.py
+++ palom/pyramid.py
@@ -12,13 +12,7 @@ from . import __version__ as _version
 
 
 class PyramidSetting:
-
-    def __init__(
-        self,
-        downscale_factor=2,
-        tile_size=1024,
-        max_pyramid_img_size=1024
-    ):
+    def __init__(self, downscale_factor=2, tile_size=1024, max_pyramid_img_size=1024):
         self.downscale_factor = downscale_factor
         self.tile_size = tile_size
         self.max_pyramid_img_size = max_pyramid_img_size
@@ -32,7 +26,7 @@ class PyramidSetting:
     def pyramid_shapes(self, base_shape):
         num_levels = self.num_levels(base_shape)
         factors = self.downscale_factor ** np.arange(num_levels)
-        shapes = np.ceil(np.array(base_shape) / factors[:,None])
+        shapes = np.ceil(np.array(base_shape) / factors[:, None])
         return [tuple(map(int, s)) for s in shapes]
 
     def num_levels(self, base_shape):
@@ -41,7 +35,7 @@ class PyramidSetting:
 
 
 def format_channel_names(num_channels_each_mosaic, channel_names):
-    '''
+    """
     format_channel_names(
         [1, 2, 3, 4, 5], ['x', 'x', ['x'], ['x', 'x']]
     )
@@ -57,11 +51,9 @@ def format_channel_names(num_channels_each_mosaic, channel_names):
         'Mosaic 4_3',
         'Mosaic 4_4'
     ]
-    '''
+    """
     matched_channel_names = []
-    for idx, (n, c) in enumerate(
-        zip(channel_names, num_channels_each_mosaic)
-    ):
+    for idx, (n, c) in enumerate(zip(channel_names, num_channels_each_mosaic)):
         nl = n
         if type(n) == str:
             nl = [n] * c
@@ -70,9 +62,7 @@ def format_channel_names(num_channels_each_mosaic, channel_names):
         if len(nl) != c:
             nl = [f"Mosaic {idx+1}"] * c
         matched_channel_names.append(nl)
-    return make_unique_str(
-        [n for l in matched_channel_names for n in l]
-    )
+    return make_unique_str([n for l in matched_channel_names for n in l])
 
 
 def make_unique_str(str_list):
@@ -80,14 +70,11 @@ def make_unique_str(str_list):
         return str_list
     else:
         max_length = max([len(s) for s in str_list])
-        str_np = np.array(str_list, dtype=np.dtype(('U', max_length+10)))
+        str_np = np.array(str_list, dtype=np.dtype(("U", max_length + 10)))
         unique, counts = np.unique(str_np, return_counts=True)
         has_duplicate = unique[counts > 1]
         for n in has_duplicate:
-            suffixes = [
-                f"_{i}"
-                for i in range(1, (str_np == n).sum()+1)
-            ]
+            suffixes = [f"_{i}" for i in range(1, (str_np == n).sum() + 1)]
             str_np[str_np == n] = np.char.add(n, suffixes)
     return make_unique_str(list(str_np))
 
@@ -118,7 +105,7 @@ def write_pyramid(
     is_mask=False,
     tile_size=None,
     save_RAM=False,
-    kwargs_tifffile=None
+    kwargs_tifffile=None,
 ):
     mosaics = normalize_mosaics(mosaics)
     ref_m = mosaics[0]
@@ -128,49 +115,45 @@ def write_pyramid(
     assert int(downscale_factor) == downscale_factor
     assert downscale_factor < min(base_shape)
     pyramid_setting = PyramidSetting(
-        downscale_factor=int(downscale_factor),
-        tile_size=max(ref_m.chunksize)
+        downscale_factor=int(downscale_factor), tile_size=max(ref_m.chunksize)
     )
     num_levels = pyramid_setting.num_levels(base_shape)
     tile_shapes = pyramid_setting.tile_shapes(base_shape)
     shapes = pyramid_setting.pyramid_shapes(base_shape)
 
     if tile_size is not None:
-        assert tile_size % 16 == 0, (
-            f"tile_size must be None or multiples of 16, not {tile_size}"
-        )
+        assert (
+            tile_size % 16 == 0
+        ), f"tile_size must be None or multiples of 16, not {tile_size}"
         tile_shapes = [(tile_size, tile_size)] * num_levels
 
     dtype = ref_m.dtype
 
-    software = f'palom {_version}'
+    software = f"palom {_version}"
     pixel_size = pixel_size
     metadata = {
-        'Creator': software,
-        'Pixels': {
-            'PhysicalSizeX': pixel_size, 'PhysicalSizeXUnit': '\u00b5m',
-            'PhysicalSizeY': pixel_size, 'PhysicalSizeYUnit': '\u00b5m'
+        "Creator": software,
+        "Pixels": {
+            "PhysicalSizeX": pixel_size,
+            "PhysicalSizeXUnit": "\u00b5m",
+            "PhysicalSizeY": pixel_size,
+            "PhysicalSizeYUnit": "\u00b5m",
         },
     }
 
     if channel_names is not None:
-        num_channels_each_mosaic = [
-            count_num_channels([m])
-            for m in mosaics
-        ]
+        num_channels_each_mosaic = [count_num_channels([m]) for m in mosaics]
         names = format_channel_names(num_channels_each_mosaic, channel_names)
         if len(names) == num_channels:
-            metadata.update({
-                'Channel': {'Name': names},
-            })
+            metadata.update(
+                {
+                    "Channel": {"Name": names},
+                }
+            )
 
     logger.info(f"Writing to {path}")
     with tifffile.TiffWriter(path, bigtiff=True) as tif:
-        kwargs = dict(
-            metadata=metadata,
-            software=software,
-            compression=compression
-        )
+        kwargs = dict(metadata=metadata, software=software, compression=compression)
         if kwargs_tifffile is None:
             kwargs_tifffile = {}
         tif.write(
@@ -181,12 +164,10 @@ def write_pyramid(
             subifds=int(num_levels - 1),
             dtype=dtype,
             tile=tile_shapes[0],
-            **{**kwargs, **kwargs_tifffile}
+            **{**kwargs, **kwargs_tifffile},
         )
-        logger.info('Generating pyramid')
-        for level, (shape, tile_shape) in enumerate(
-            zip(shapes[1:], tile_shapes[1:])
-        ):
+        logger.info("Generating pyramid")
+        for level, (shape, tile_shape) in enumerate(zip(shapes[1:], tile_shapes[1:])):
             if verbose:
                 logger.info(f"    Level {level+1} ({shape[0]} x {shape[1]})")
             tif.write(
@@ -197,27 +178,21 @@ def write_pyramid(
                     downscale_factor=downscale_factor,
                     level=level,
                     is_mask=is_mask,
-                    save_RAM=save_RAM
+                    save_RAM=save_RAM,
                 ),
                 shape=(num_channels, *shape),
                 subfiletype=1,
                 dtype=dtype,
                 tile=tile_shape,
-                **{
-                    **dict(compression=compression),
-                    **kwargs_tifffile
-                }
+                **{**dict(compression=compression), **kwargs_tifffile},
             )
 
 
 def count_num_channels(imgs):
     for img in imgs:
         assert img.ndim == 2 or img.ndim == 3
-    return sum([
-        1 if img.ndim == 2 else img.shape[0]
-        for img in imgs
-    ])
-   
+    return sum([1 if img.ndim == 2 else img.shape[0] for img in imgs])
+
 
 def tile_from_combined_mosaics(mosaics, tile_shape, save_RAM=False):
     num_rows, num_cols = mosaics[0].shape[1:3]
@@ -237,7 +212,7 @@ def tile_from_combined_mosaics(mosaics, tile_shape, save_RAM=False):
                 c = da_to_zarr(c) if save_RAM else c.compute()
             for y in range(0, num_rows, h):
                 for x in range(0, num_cols, w):
-                    yield np.array(c[y:y+h, x:x+w])
+                    yield np.array(c[y : y + h, x : x + w])
                     # yield m[y:y+h, x:x+w].copy().compute()
             c = None
 
@@ -249,15 +224,15 @@ def tile_from_pyramid(
     downscale_factor=2,
     level=0,
     is_mask=False,
-    save_RAM=False
+    save_RAM=False,
 ):
     # workaround progress bar
     # https://forum.image.sc/t/tifffile-ome-tiff-generation-is-taking-too-much-ram/41865/26
-    pbar = tqdm.tqdm(total=num_channels, ascii=True, desc=f'Processing channel')
+    pbar = tqdm.tqdm(total=num_channels, ascii=True, desc=f"Processing channel")
     for c in range(num_channels):
-        img = da.from_zarr(zarr.open(tifffile.imread(
-            path, series=0, level=level, aszarr=True
-        )))
+        img = da.from_zarr(
+            zarr.open(tifffile.imread(path, series=0, level=level, aszarr=True))
+        )
         if img.ndim == 2:
             img = img.reshape(1, *img.shape)
         img = img[c]
@@ -265,8 +240,7 @@ def tile_from_pyramid(
         # img = tifffile.imread(path, series=0, level=level, key=c)
         if not is_mask:
             img = img.map_blocks(
-                cv2.blur,
-                ksize=(downscale_factor, downscale_factor), anchor=(0, 0)
+                cv2.blur, ksize=(downscale_factor, downscale_factor), anchor=(0, 0)
             )
         img = da_to_zarr(img) if save_RAM else img.compute()
         num_rows, num_columns = img.shape
@@ -282,7 +256,9 @@ def tile_from_pyramid(
                     pbar.update(1)
                     if c == last_c:
                         pbar.close()
-                yield np.array(img[y:y+h:downscale_factor, x:x+w:downscale_factor])
+                yield np.array(
+                    img[y : y + h : downscale_factor, x : x + w : downscale_factor]
+                )
         # setting img to None seems necessary to prevent RAM spike
         img = None
 
@@ -294,12 +270,7 @@ def da_to_zarr(da_img, zarr_store=None, num_workers=None, out_shape=None, chunks
         if chunks is None:
             chunks = da_img.chunksize
         zarr_store = zarr.create(
-            out_shape,
-            chunks=chunks,
-            dtype=da_img.dtype,
-            overwrite=True
+            out_shape, chunks=chunks, dtype=da_img.dtype, overwrite=True
         )
-    da_img.to_zarr(zarr_store, compute=False).compute(
-        num_workers=num_workers
-    )
+    da_img.to_zarr(zarr_store, compute=False).compute(num_workers=num_workers)
     return zarr_store
